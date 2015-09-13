15652ce10af61fb99bfc898d00e2a305032d80ca
nickgallimore <nfgallimore@gmail.com>
2015-09-12 21:41:28 -0400

    Initial Commit

---
+++ hellworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hellworld
+//
+//  Created by Nicholas Gallimore on 9/12/15.
+//  Copyright © 2015 Nicholas Gallimore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
